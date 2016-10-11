ae08a7666532e1ff071a1f990987a7fdc0941f67
jinimcoroneo <jinim1983@gmail.com>
2016-10-11 18:52:51 +0100

    step 18, entering players

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb :index
   end
 
+  post '/names' do
+    @name_1 = params[:name_1]
+    @name_2 = params[:name_2]
+    erb :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features_spec.rb
@@ -0,0 +1,10 @@
+
+feature 'Enter names' do
+  scenario 'choosing heroes' do
+    visit('/')
+    fill_in :name_1, with: "Peter"
+    fill_in :name_2, with: "Stan"
+    click_button 'touch me, I know you want to'
+    expect(page).to have_content "Peter VS Stan"
+  end
+end
