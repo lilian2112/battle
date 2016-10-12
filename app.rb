c5cb4dcf4043c8161b3e5f8c9ab348966a5120d4
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-12 11:52:04 +0100

    Completed step 19 - added redirect and enabled sessions

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @name_1 = params[:name_1]
-    @name_2 = params[:name_2]
+    session["name_1"] = params[:name_1]
+    session["name_2"] = params[:name_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name_1 = session[:name_1]
+    @name_2 = session[:name_2]
     erb :play
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -5,6 +5,7 @@ feature 'Enter names' do
     fill_in :name_1, with: "Peter"
     fill_in :name_2, with: "Stan"
     click_button 'touch me, I know you want to'
-    expect(page).to have_content "Peter VS Stan"
+    expect(page).to have_content "Peter"
+    expect(page).to have_content "Stan"
   end
 end
